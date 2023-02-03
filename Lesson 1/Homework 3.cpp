779f257e6625b22f33f817c2dc22fbd1b7bc6e84
Michaelwucoc <goodluckmichael0@163.com>
2023-02-03 22:16:07 +0800

    New

--- Lesson 1/Homework 3.cpp
+++ Lesson 1/Homework 3.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"   * * * *"<<endl;
-    cout<<"  *     *"<<endl;
-    cout<<" *     *"<<endl;
-    cout<<"* * * *";
+    cout<<"  +---+"<<endl;
+    cout<<" /   /|"<<endl;
+    cout<<"+---+ |"<<endl;
+    cout<<"|   | +"<<endl;
+    cout<<"|   |/"<<endl;
+    cout<<"+---+";
 
     return 0;
     
