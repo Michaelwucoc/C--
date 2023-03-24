12e6088f1eba0973af15fb2ec3664e52df33355a
Michaelwucoc <goodluckmichael0@163.com>
2023-03-24 19:13:40 +0800

    yee

---
+++ Lesson 3/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a;
+	cin>>a;
+	if(a>=90)
+	{
+		cout<<"Yes";
+	}
+	else
+	{
+		cout<<"No";
+	}
+}
---
+++ Lesson 3/2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a, b, c;
+	cin >> a >> b;
+	if (a < b){
+		c =a;
+        a = b;
+        b = c;
+        cout << a <<" "<< b;
+	}
+    else{
+    	cout << a <<" "<<b;
+    }
+	return 0;
+}
