12acb8c9d72011993339fb9344e2ff33ad49bb84
Michaelwucoc <goodluckmichael0@163.com>
2023-03-24 19:13:27 +0800

    emmm

---
+++ Lesson 5/2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	
+	int sum=0;
+	
+	for(int i=1; ;i++){
+		for(int j=1;j<=i && ?;j++){
+			
+		}
+	}
+	cout<<sum;
+	
+	return 0;
+}
